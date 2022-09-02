449a030b6d2645d63952d271c5868540def81a68
Andrés Torres Albuja <bryan.torres01@epn.edu.ec>
2022-09-02 12:00:25 -0500

    Router outlet y configuraciones generales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'comics',
+    loadChildren: () => import('./modules/comic/comic.module').then(module => module.ComicModule),
+  },
+  {
+    path: '',
+    redirectTo: 'comics',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'comics',
+    pathMatch: 'full',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
